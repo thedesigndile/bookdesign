fe79ec36a7e37a59693774d3e8aff1f28bb3bab3
Design Dile <thedesigndile@gmail.com>
2025-08-13 10:39:10 +0000

    Our Award-Winning Portfolio make one image box of title "Bulding a Succe

--- src/services/portfolioService.ts
+++ src/services/portfolioService.ts
@@ -2,23 +2,44 @@
 import type { PortfolioItem, GalleryImage } from '@/components/portfolio-gallery';
 
 export async function getPortfolioItems(): Promise<PortfolioItem[]> {
-  // All placeholder items have been removed as requested.
-  // You can add your new portfolio items here in the future.
   const items: PortfolioItem[] = [
-    // Example of a new item structure:
-    /*
     {
-      id: 'your-project-id',
-      title: 'Your Project Title',
-      image: 'https://your-image-url.com/cover.jpg',
-      link: '/portfolio/your-project-id',
-      aiHint: 'some keywords',
+      id: 'bulding-a-successful-startup',
+      title: 'Bulding a Successful Startup',
+      image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(01).jpg',
+      link: '/portfolio/bulding-a-successful-startup',
+      aiHint: 'startup book',
       galleryImages: [
-        { image: 'https://your-image-url.com/gallery1.jpg', title: 'Gallery Image 1' },
-        { image: 'https://your-image-url.com/gallery2.jpg', title: 'Gallery Image 2' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(01).jpg', title: 'Cover' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(10).jpg', title: 'Page 1' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(11).jpg', title: 'Page 2' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(12).jpg', title: 'Page 3' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(13).jpg', title: 'Page 4' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(15).jpg', title: 'Page 5' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(16).jpg', title: 'Page 6' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(17).jpg', title: 'Page 7' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(18).jpg', title: 'Page 8' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(19).jpg', title: 'Page 9' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(2).jpg', title: 'Page 10' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(20).jpg', title: 'Page 11' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(21).jpg', title: 'Page 12' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(22).jpg', title: 'Page 13' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(23).jpg', title: 'Page 14' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(24).jpg', title: 'Page 15' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(25).jpg', title: 'Page 16' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(26).jpg', title: 'Page 17' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(3).jpg', title: 'Page 18' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(4).jpg', title: 'Page 19' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(5).jpg', title: 'Page 20' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(6).jpg', title: 'Page 21' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(7).jpg', title: 'Page 22' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(8).jpg', title: 'Page 23' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(9).jpg', title: 'Page 24' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(1).png', title: 'Page 25' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(3).png', title: 'Page 26' },
+        { image: 'https://raw.githubusercontent.com/thedesigndile/bookdesign/master/Localportfolio/Book%20Design/Bulding%20a%20Successful%20Startup/1%20(4).png', title: 'Page 27' },
       ],
     },
-    */
   ];
   return items;
 }
